e4379c1fd7ab3342d943706d65c8c081419cba28
SPC <SPC@23-LAB402>
2024-07-17 18:53:33 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Ứng dụng giải phương trình bậc 1");
+
+        double a = 0, b = 0;
+        string ketqua = GiaiPTBac1(a, b);
+        Console.WriteLine($"Ứng dụng giai phương trình bậc 1 {ketqua}");
+           
+    }
+    private static string GiaiPTBac1(double a, double b)
+    {
+        //
+        return "asssss";
+    }
+}
